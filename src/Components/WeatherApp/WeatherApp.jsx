22676ce07032d86dfcfcbea31bdc67358c00b729
Abhishek kumar Dubey <akdubey.dtg@gmail.com>
2023-11-15 16:09:49 +0530

    second commit

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -8,6 +8,10 @@ import rain_icon from '../Assets/rain.png'
 import snow_icon from '../Assets/snow.png'
 import wind_icon from '../Assets/wind.png'
 import drizzle_icon from '../Assets/drizzle.png'
+import AirPressure_icon from '../Assets/AirPressure.png'
+import GroundLevel_icon from '../Assets/GroundLevel.png'
+import SeaLevel_icon from '../Assets/SeaLevel.png'
+import Description_icon from '../Assets/Discription.png'
 
 const WeatherApp = () => {
 
@@ -28,11 +32,19 @@ const WeatherApp = () => {
         const wind = document.getElementsByClassName("wind-rate")
         const temprature = document.getElementsByClassName("weather-temp")
         const location = document.getElementsByClassName("weather-location")
+        const sealevel = document.getElementsByClassName("sea-level")
+        const groundlevel = document.getElementsByClassName("ground-level")
+        const pressure = document.getElementsByClassName("Pressure")
+        const description = document.getElementsByClassName("Description")
 
         humidity[0].innerHTML = data.main.humidity + "%";
         wind[0].innerHTML = Math.floor(data.wind.speed) + "km/h";
         temprature[0].innerHTML = Math.floor(data.main.temp) + "°C";
         location[0].innerHTML = data.name;
+        sealevel[0].innerHTML = data.main.sea_level + "m";
+        groundlevel[0].innerHTML = data.main.grnd_level + "m";
+        pressure[0].innerHTML = data.main.pressure + "P";
+        description[0].innerHTML = data.weather[0].description ;
 
         if(data.weather[0].icon === "01d" || data.weather[0].icon === "01n")
         {
@@ -79,11 +91,16 @@ const WeatherApp = () => {
         </div>
 
 
+        <div className="box">
         <div className="weather-image">
             <img src={wicon} alt="" />
         </div>
         <div className="weather-temp">24°C</div>
+        </div>
         <div className="weather-location">India</div>
+
+
+        <div className="data-containerbox">
         <div className="data-container">
             <div className="element">
               <img src={humidity_icon} alt="" className="icon" />
@@ -91,6 +108,8 @@ const WeatherApp = () => {
                 <div className="humidity-percent">64%</div>
                 <div className="text">Humidity</div>
               </div>
+                
+                
             </div>
             <div className="element">
               <img src={wind_icon} alt="" className="icon" />
@@ -99,7 +118,47 @@ const WeatherApp = () => {
                 <div className="text">Wind Speed</div>
               </div>
             </div>
+            <div className="element">
+              <img src={AirPressure_icon} alt="" className="icon" />
+              <div className="data">
+                <div className="Pressure">718 P</div>
+                <div className="text">Pressure</div>
+              </div>
+            </div>
+           
         </div>
+        <div className="data-container">
+            <div className="element">
+              <img src={SeaLevel_icon} alt="" className="icon" />
+              <div className="data">
+                <div className="sea-level">200m</div>
+                <div className="text">Sea Level</div>
+              </div>
+                
+                
+            </div>
+            <div className="element">
+              <img src={GroundLevel_icon} alt="" className="icon" />
+              <div className="data">
+                <div className="ground-level">500m</div>
+                <div className="text">Ground Level</div>
+              </div>
+            </div>
+        </div>
+        </div>
+
+        <div className="text-area">
+            
+                <img src={Description_icon} className='icon1' alt="" />
+                <div className="desbox">
+              
+                <div className="text1">Description:-</div>
+                <div className="Description">.......</div>
+                </div>
+            
+
+        </div>
+        
       
     </div>
   )
